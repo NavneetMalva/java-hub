5f64985f36fadfcea39f585a9d3d34bf82988af0
Navneet Malva <navneetmalva@gmail.com>
2024-09-11 21:20:04 +0530

    package array updated

---
+++ JavaCodingPractice/src/array/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+
+		Integer[] numbers = { 1, 2, 3, 1, 2, 4, 5 };
+		Set<Integer> hs = new HashSet<>();
+
+		for (int num : numbers) {
+			hs.add(num);
+		}
+
+		System.out.println("Array after duplication removed: " + hs); // [1, 2, 3, 4, 5]
+
+		Integer[] result = hs.toArray(new Integer[hs.size()]);
+		System.out.println("Array after duplication removed: " + Arrays.toString(result)); // [1, 2, 3, 4, 5]
+
+	}
+
+}
