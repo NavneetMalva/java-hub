9e112eceb62bf5001eadf72e806a924fb466bcda
Navneet Malva <navneetmalva@gmail.com>
2024-09-12 14:48:26 +0530

    Updated

---
+++ JavaCodingPractice/src/common/PalindromeEx02.java
@@ -0,0 +1,33 @@
+package common;
+
+import java.util.Scanner;
+
+public class PalindromeEx02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		String str = scan.nextLine();
+		scan.close();
+
+		if (isPalindrome(str)) {
+			System.out.println(str + " is a palindrome.");
+		} else {
+			System.out.println(str + " is not a palindrome.");
+		}
+
+	}
+
+	public static boolean isPalindrome(String str) {
+		int i = 0;
+		int j = str.length() - 1;
+		while (i < j) {
+			if (str.charAt(i) != str.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+
+}
