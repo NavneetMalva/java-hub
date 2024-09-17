14c82efb20562ce400a2f81bc5b442b34c5751ec
Navneet Malva <navneetmalva@gmail.com>
2024-09-17 09:33:43 +0530

    jdbc & jpa springboot sample application

---
+++ SpringBoot-Projects/database-demo/src/main/java/com/springboot/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,37 @@
+package com.springboot.databasedemo;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.springboot.databasedemo.entity.Person;
+import com.springboot.databasedemo.jdbc.PersonJdbcDao;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao personDao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", personDao.findAll());
+		logger.info("User details -> {}", personDao.findById(10001));
+		logger.info("Deleting user -> No. of Rows deleted ->{}", personDao.deleteById(10002));
+		logger.info("Inserting 10004 -> {}", personDao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		logger.info("Update 10003 -> {}", personDao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+
+	}
+
+}
---
+++ SpringBoot-Projects/database-demo/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+create table person
+(
+   id integer not null,
+   name varchar(255) not null,
+   location varchar(255),
+   birth_date timestamp,
+   primary key(id)
+);
+
+
+INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE)
+   VALUES (10001, 'Rick', 'NYC', CURRENT_DATE()),
+   (10002,'Mokito','Tokyo', CURRENT_DATE()),
+   (10003,'Morty','Texas',CURDATE());
