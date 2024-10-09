74f9abda6464d8d28458a5bc4d740ef2d9d06510
NavneetMalva <navneetmalva@gmail.com>
2024-10-09 13:53:17 +0530

    Updated

---
+++ JavaCodingPractice/src/array/ReverseSubArray.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReverseSubArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7};
+
+        // after which position we want to reverse the array
+        int m =3;
+
+        int start = m+1;
+        int end= arr.length-1;
+
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
--- JavaCodingPractice/src/exceptions/ExceptionHandlingEx02.java
+++ JavaCodingPractice/src/exceptions/ExceptionHandlingEx02.java
@@ -3,6 +3,11 @@ package exceptions;
 /* how to create custom exception...
 * pass the arguments to run this program
 * e.g java ExceptionHandlingEx02 50 20
+*
+* To compile this
+* cd src/exceptions , javac ExceptionHandlingEx02.java
+* Now to Run this
+* cd src, java exceptions/ExceptionHandlingEx02 50 20
 */
 
 class AgeInvalidException extends Exception
