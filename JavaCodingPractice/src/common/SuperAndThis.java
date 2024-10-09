3c1d479dcbf85ba5e5e3386f78fe27c8031dce54
NavneetMalva <navneetmalva@gmail.com>
2024-10-09 21:08:50 +0530

    Updated

---
+++ JavaCodingPractice/src/common/SuperAndThis.java
@@ -0,0 +1,36 @@
+package common;
+
+/**
+ * We can use this as well as super anywhere except static area.
+ * if  we use this as well as super inside public static void main(String[]args)
+ *  we will get Compile Time Error since cannot use them inside static area.
+ * We can use this as well as super any number of times in a program.
+ */
+
+class Animal {
+    String type;
+
+    Animal(String type) {
+        this.type = type;
+    }
+}
+
+class Dog extends Animal {
+    String sound;
+
+    Dog(String type, String sound) {
+        super(type); // invoking superclass constructor
+        this.sound = sound; // using 'this' to refer to the current object's instance variable
+    }
+}
+
+public class SuperAndThis {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Husky");
+        System.out.println(animal.type);
+
+        Dog dog = new Dog(animal.type, "Bark");
+        System.out.println(dog.type+" "+ dog.sound);
+
+    }
+}
---
+++ JavaCodingPractice/src/oops/abstraction/InterfaceEx01.java
@@ -0,0 +1,33 @@
+package oops.abstraction;
+
+/**
+ * The interface in Java is a mechanism to achieve abstraction.
+ * There can be only abstract methods in the Java interface, not method body. It is used to achieve abstraction and multiple inheritance in Java.
+ *
+ * In other words, you can say that interfaces can have abstract methods and variables. It cannot have a method body.
+ *
+ * Since Java 8, we can have default and static methods in an interface.
+ *
+ * Since Java 9, we can have private methods in an interface.
+ */
+
+interface Drawable{
+    void draw(); // abstract method
+    default void msg(){System.out.println("default method");} // default method
+    static int cube(int x){return x*x*x;} // static method, belongs to the class rather than instance
+}
+class Rectangles implements Drawable{
+    public void draw(){System.out.println("drawing rectangle");}
+}
+
+public class InterfaceEx01 {
+    public static void main(String[] args) {
+        Rectangles r = new Rectangles();
+        r.draw();
+        r.msg();
+
+        System.out.println(Drawable.cube(10));
+
+
+    }
+}
