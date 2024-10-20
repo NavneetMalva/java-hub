55aad6a8bb932a59d206c45817e898b6553f4ec3
NavneetMalva <navneetmalva@gmail.com>
2024-10-20 13:24:42 +0530

    impQ added

---
+++ JavaCodingPractice/src/info/HashMapQ.java
@@ -0,0 +1,29 @@
+package info;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapQ {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+
+        map.put(1,"John");
+        map.put(2,"Jane");
+        map.put(null,"Rick");
+        map.put(null,"Mary");
+        map.put(null, null);
+
+        /**
+         * Even though map can only have one null key, it will compile and run fine
+         * just that in output we will get only one outout will null key with the last entry
+         * null null
+         * 1 John
+         * 2 Jane
+         */
+
+        for(Map.Entry<Integer,String> entry : map.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+
+    }
+}
---
+++ JavaCodingPractice/src/info/TreeMapQ.java
@@ -0,0 +1,25 @@
+package info;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapQ {
+    public static void main(String[] args) {
+
+        Map<Integer,String> tp = new TreeMap<>();
+        tp.put(1,"Rick");
+        tp.put(2,"Bob");
+
+        // what will happen if we try to insert null key in "TreeMap"
+        /**
+         * NullPointerException  on Runtime
+         */
+        tp.put(null,"Jack");
+
+        for(Map.Entry<Integer,String> entry : tp.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+
+    }
+}
